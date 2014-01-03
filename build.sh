eb7506204f07dee337e96ed5c903475c7557c2e0
skybluecircles <william@skybluecircles.com>
2014-01-03 06:10:55 -0500

    Update build.sh
    didn't seem like cd was necessary

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 nasm src/bootsectors/bmfs_mbr.asm -o bmfs_mbr.sys
 nasm src/bootsectors/pxestart.asm -o pxestart.sys
-cd src
-nasm pure64.asm -o ../pure64.sys
-cd ..
+nasm src/pure64.asm -o pure64.sys
+
+exit 0
