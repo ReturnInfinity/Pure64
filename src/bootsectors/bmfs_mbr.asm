047f846d5901276acd7b57ad8599c87b341c5017
Ian Seyler <ian@seyler.me>
2016-12-04 20:11:07 -0500

    Improved MBR
    This code was provided by Christian Müller.

--- src/bootsectors/bmfs_mbr.asm
+++ src/bootsectors/bmfs_mbr.asm
@@ -7,8 +7,14 @@
 ;
 ; In this code we are expecting a BMFS-formatted drive. With BMFS the Pure64
 ; binary is required to start at sector 16 (8192 bytes from the start). A small
-; ckeck is made to make sure Pure64 was loaded by comparing a signiture.
+; check is made to make sure Pure64 was loaded by comparing a signiture.
 ; =============================================================================
+
+
+%define SECTORS 64
+%define STARTSECTOR 16
+%define ADDRESS 0x8000
+%define SEGMENT 0x0000
 
 USE16
 org 0x7C00
@@ -100,15 +106,10 @@ check_A20:
 	mov si, msg_Load
 	call print_string_16
 
-	mov eax, 64			; Number of sectors to load. 64 sectors = 32768 bytes
-	mov ebx, 16			; Start immediately after directory (offset 8192)
-	mov cx, 0x8000			; Pure64 expects to be loaded at 0x8000
-
-load_nextsector:
-	call readsector			; Load 512 bytes
-	dec eax
-	cmp eax, 0
-	jnz load_nextsector
+	mov ah, 0x42			; Extended Read
+	mov dl, [DriveNumber]		; http://www.ctyme.com/intr/rb-0708.htm
+	mov si, DAP
+	int 0x13
 
 	mov eax, [0x8000]
 	cmp eax, 0x00018BE9		; Match against the Pure64 binary
@@ -128,69 +129,6 @@ magic_fail:
 halt:
 	hlt
 	jmp halt
-
-;------------------------------------------------------------------------------
-; Read a sector from a disk, using LBA
-; IN:	EAX - High word of 64-bit DOS sector number
-;	EBX - Low word of 64-bit DOS sector number
-;	ES:CX - destination buffer
-; OUT:	ES:CX points one byte after the last byte read
-;	EAX - High word of next sector
-;	EBX - Low word of sector
-readsector:
-	push eax
-	xor eax, eax			; We don't need to load from sectors > 32-bit
-	push dx
-	push si
-	push di
-
-read_it:
-	push eax			; Save the sector number
-	push ebx
-	mov di, sp			; remember parameter block end
-
-	push eax			; [C] sector number high 32bit
-	push ebx			; [8] sector number low 32bit
-	push es				; [6] buffer segment
-	push cx				; [4] buffer offset
-	push byte 1			; [2] 1 sector (word)
-	push byte 16			; [0] size of parameter block (word)
-
-	mov si, sp
-	mov dl, [DriveNumber]
-	mov ah, 42h			; EXTENDED READ
-	int 0x13			; http://hdebruijn.soo.dto.tudelft.nl/newpage/interupt/out-0700.htm#0651
-
-	mov sp, di			; remove parameter block from stack
-	pop ebx
-	pop eax				; Restore the sector number
-
-	jnc read_ok			; jump if no error
-
-	push ax
-	xor ah, ah			; else, reset and retry
-	int 0x13
-	pop ax
-	jmp read_it
-
-read_ok:
-	add ebx, 1			; increment next sector with carry
-	adc eax, 0
-	add cx, 512			; Add bytes per sector
-	jnc no_incr_es			; if overflow...
-
-incr_es:
-	mov dx, es
-	add dh, 0x10			; ...add 1000h to ES
-	mov es, dx
-
-no_incr_es:
-	pop di
-	pop si
-	pop dx
-	pop eax
-
-	ret
 ;------------------------------------------------------------------------------
 
 
@@ -231,6 +169,13 @@ times 446-$+$$ db 0
 ; False partition table entry required by some BIOS vendors.
 db 0x80, 0x00, 0x01, 0x00, 0xEB, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF
 DriveNumber db 0x00
+
+DAP:	db 0x10
+	db 0x00
+	dw SECTORS
+	dw ADDRESS
+	dw SEGMENT
+	dq STARTSECTOR
 
 times 510-$+$$ db 0
 
