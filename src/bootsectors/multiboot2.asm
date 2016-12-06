0eaf8158271665d83212955e7bd056c8426549a3
Ian Seyler <ian.seyler@returninfinity.com>
2016-12-06 11:04:49 -0500

    Update multiboot2.asm

--- src/bootsectors/multiboot2.asm
+++ src/bootsectors/multiboot2.asm
@@ -8,9 +8,18 @@ ARCHITECHTURE		equ 0
 HEADER_LENGTH		equ multiboot_header_end - multiboot_header
 CHECKSUM		equ -(MAGIC + ARCHITECHTURE + HEADER_LENGTH)
 
+_start:				; We need some code before the multiboot header
+	xor eax, eax		; Clear eax and ebx in the event
+	xor ebx, ebx		; we are not loaded by GRUB.
+	jmp multiboot_entry	; Jump over the multiboot header
+	align 8			; Multiboot 2 header must be 64-bit aligned
+
 multiboot_header:
 	dd MAGIC
 	dd ARCHITECHTURE
 	dd HEADER_LENGTH
 	dd CHECKSUM
 multiboot_header_end:
+
+multiboot_entry:
+	jmp $
