7b378bdff7565661fa7c8d2728ad104f72707b6d
Ian Seyler <ian@seyler.me>
2016-12-07 20:22:14 -0500

    Update multiboot2

--- src/bootsectors/multiboot2.asm
+++ src/bootsectors/multiboot2.asm
@@ -2,11 +2,16 @@
 ; http://nongnu.askapache.com/grub/phcoder/multiboot.pdf
 
 [BITS 32]
+[global _start]
+[ORG 0x100000]		;If using '-f bin' we need to specify the
+			;origin point for our code with ORG directive
+			;multiboot loaders load us at physical
+			;address 0x100000
 
 MAGIC			equ 0xE85250D6
 ARCHITECHTURE		equ 0
-HEADER_LENGTH		equ multiboot_header_end - multiboot_header
-CHECKSUM		equ -(MAGIC + ARCHITECHTURE + HEADER_LENGTH)
+HEADER_LENGTH		equ multiboot_header_end - multiboot_header_start
+CHECKSUM		equ 0x100000000 - (MAGIC + ARCHITECHTURE + HEADER_LENGTH)
 
 _start:				; We need some code before the multiboot header
 	xor eax, eax		; Clear eax and ebx in the event
@@ -14,7 +19,7 @@ _start:				; We need some code before the multiboot header
 	jmp multiboot_entry	; Jump over the multiboot header
 	align 8			; Multiboot 2 header must be 64-bit aligned
 
-multiboot_header:
+multiboot_header_start:
 	dd MAGIC
 	dd ARCHITECHTURE
 	dd HEADER_LENGTH
@@ -23,12 +28,23 @@ entry_address_tag_start:
 	dw 3
 	dw 0
 	dd entry_address_tag_end - entry_address_tag_start
-	dd multiboot_entry
+	dq multiboot_entry
 entry_address_tag_end:
+framebuffer_tag_start:
+        dw 5
+        dw 0
+        dd framebuffer_tag_end - framebuffer_tag_start
+        dd 800
+        dd 600
+        dd 32
+framebuffer_tag_end:
 	dw 0			; End type
 	dw 0
 	dd 8
 multiboot_header_end:
 
 multiboot_entry:
+	cmp eax, 0x36D76289	; Magic value
+	jne error
+error:
 	jmp $
