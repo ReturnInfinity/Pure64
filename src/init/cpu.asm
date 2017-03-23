4cbe3ccc70f8d8ffcb998c4fd5729ed00712b46e
Ian Seyler <ian.seyler@returninfinity.com>
2017-03-23 13:43:18 -0400

    Update cpu.asm

--- src/init/cpu.asm
+++ src/init/cpu.asm
@@ -2,7 +2,7 @@
 ; Pure64 -- a 64-bit OS/software loader written in Assembly for x86-64 systems
 ; Copyright (C) 2008-2017 Return Infinity -- see LICENSE.TXT
 ;
-; INIT CPU
+; INIT CPU - This code is called by all activated CPU cores in the system
 ; =============================================================================
 
 
