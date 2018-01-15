3879fd23b77c1ef92d5d00c3f2a1ddaf13c1474c
Taylor Holberton <taylorcholberton@gmail.com>
2018-01-15 13:01:46 -0500

    added missing prototypes

--- include/pure64/stream.h
+++ include/pure64/stream.h
@@ -9,7 +9,6 @@
 #ifndef PURE64_STREAM_H
 #define PURE64_STREAM_H
 
-#include <stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -23,15 +22,15 @@ struct pure64_stream {
 	/** Implementation data */
 	void *data;
 	/** Get size callback */
-	bool (*get_size)(void *data, uint64_t *size);
+	int (*get_size)(void *data, uint64_t *size);
 	/** Get position callback */
-	bool (*get_pos)(void *data, uint64_t *offset);
+	int (*get_pos)(void *data, uint64_t *offset);
 	/** Read callback */
-	bool (*read)(void *data, void *buf, uint64_t buf_size);
+	int (*read)(void *data, void *buf, uint64_t buf_size);
 	/** Set position callback */
-	bool (*set_pos)(void *data, uint64_t offset);
+	int (*set_pos)(void *data, uint64_t offset);
 	/** Write callback */
-	bool (*write)(void *data, const void *buf, uint64_t buf_size);
+	int (*write)(void *data, const void *buf, uint64_t buf_size);
 };
 
 /** Initializes the stream for use.
@@ -39,6 +38,54 @@ struct pure64_stream {
  * */
 
 void pure64_stream_init(struct pure64_stream *stream);
+
+/** Get the size of the stream. If
+ * the stream is resizeable, then it is
+ * the span of the stream that actually
+ * contains data.
+ * @param stream An initialized stream structure.
+ * @param size The pointer to the variable that
+ * will receive the size of the stream.
+ * @returns Zero on success, non-zero on failure.
+ * */
+
+int pure64_stream_get_size(struct pure64_stream *stream, uint64_t *size);
+
+/** Gets the current position of the stream.
+ * @param stream An initialized stream structure.
+ * @param pos A pointer to the variable that will
+ * receive the current position of the stream.
+ * @returns Zero on success, non-zero on failure.
+ * */
+
+int pure64_stream_get_pos(struct pure64_stream *stream, uint64_t *pos);
+
+/** Reads data from the stream.
+ * @param stream An initialized stream structure.
+ * @param buf The buffer to put the data in.
+ * @param buf_size The number of bytes to read and
+ * the size of the buffer.
+ * @returns Zero on success, non-zero on failure.
+ * */
+
+int pure64_stream_read(struct pure64_stream *stream, void *buf, uint64_t buf_size);
+
+/** Sets the position of the stream.
+ * @param stream An initialized stream structure.
+ * @param pos The position to set the stream to.
+ * @returns Zero on success, non-zero on failure.
+ * */
+
+int pure64_stream_set_pos(struct pure64_stream *stream, uint64_t pos);
+
+/** Writes data to the stream.
+ * @param stream An initialized stream structure.
+ * @param buf The buffer containing the data.
+ * @param buf_size The number of bytes in the buffer.
+ * @returns Zero on success, non-zero on failure.
+ * */
+
+int pure64_stream_write(struct pure64_stream *stream, const void *buf, uint64_t buf_size);
 
 #ifdef __cplusplus
 } /* extern "C" { */
