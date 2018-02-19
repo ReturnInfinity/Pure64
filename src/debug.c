41b32218c62ede72558d2ae08ab3d8acbeaea6b0
Taylor Holberton <taylorcholberton@gmail.com>
2018-02-19 12:29:59 -0500

    added support for %x and %s fields, with flags and such

--- src/debug.c
+++ src/debug.c
@@ -6,6 +6,8 @@
 
 #include "debug.h"
 
+#include <stdarg.h>
+
 static void outb(unsigned short int port, unsigned char value) {
 	asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
 }
@@ -27,9 +29,407 @@ void serial_putc(char c) {
 	outb(COM1, c);
 }
 
-void debug(const char *fmt, ...) {
-	while (*fmt) {
-		serial_putc(*fmt);
-		fmt++;
+static int pure64_isdigit(char c) {
+	if ((c >= '0') && (c <= '9'))
+		return 1;
+	else
+		return 0;
+}
+
+static unsigned int pure64_strlen(const char *str) {
+
+	unsigned int len = 0;
+
+	while (str[len] != 0)
+		len++;
+
+	return len;
+}
+
+static void debug_putc(char c) {
+	serial_putc(c);
+}
+
+/** Data type is char */
+#define LENGTH_hh 0x01
+
+/** Data type is short int */
+#define LENGTH_h 0x02
+
+/** Data type is long int */
+#define LENGTH_l 0x03
+
+/** Data type is long long int */
+#define LENGTH_ll 0x04
+
+/** Data type is intmax_t or uintmax_t */
+#define LENGTH_j 0x05
+
+/** Data type is size_t */
+#define LENGTH_z 0x06
+
+/** Data type is ptrdiff_t */
+#define LENGTH_t 0x07
+
+/** Data type is long double */
+#define LENGTH_L 0x08
+
+/** Left justify the field width. */
+#define FLAG_LEFT_JUSTIFY 0x01
+
+/** Add a '+' or '-' sign whenever
+ * appropriate. */
+#define FLAG_ADD_SIGN 0x02
+
+/** If no sign is going to be printed,
+ * add a space instead. */
+#define FLAG_SPACE_OR_SIGN 0x04
+
+/** For hex fields, precede the number
+ * with 0, 0x, or 0X. For floating point
+ * numbers, it forces a decimal point to
+ * be written. */
+#define FLAG_HASH_TAG 0x08
+
+/** Print the leading zeros of the number,
+ * if any. */
+#define FLAG_PAD_ZERO 0x10
+
+/** Decimal integer */
+#define SPECIFIER_d 0x01
+
+/** Decimal whole number */
+#define SPECIFIER_u 0x02
+
+/** Hexidecimal (lower case) */
+#define SPECIFIER_x 0x03
+
+/** Hexidecimal (upper case) */
+#define SPECIFIER_X 0x04
+
+/** An ASCII character */
+#define SPECIFIER_c 0x05
+
+/** A string of ASCII characters */
+#define SPECIFIER_s 0x06
+
+/** A pointer value */
+#define SPECIFIER_p 0x07
+
+struct fmt_info {
+	/** A bit mask containing
+	 * flags from the format string. */
+	unsigned int flags;
+	/** Number of characters
+	 * to print. Zero means that
+	 * the width was not specified. */
+	unsigned int width;
+	/** If non-zero, the width is
+	 * given in an argument. */
+	unsigned int width_arg;
+	/** If non-zero, the width was
+	 * set in either an argument or
+	 * the format specifier. */
+	unsigned int width_set;
+	/** The precision of the field. */
+	unsigned int precision;
+	/** If non-zero, the precision is
+	 * given in an argument. */
+	unsigned int precision_arg;
+	/** If non-zero, it means that the
+	 * precision was set in either the
+	 * format specifier or an argument. */
+	unsigned int precision_set;
+	/** A length specifier from the
+	 * format string. */
+	unsigned int length;
+	/** If non-zero, it means that the
+	 * length was set in the format string.
+	 * */
+	unsigned int length_set;
+	/** The main specifier of the statement. */
+	unsigned int specifier;
+};
+
+static void fmt_info_init(struct fmt_info *fmt_info) {
+	fmt_info->flags = 0;
+	fmt_info->width = 0;
+	fmt_info->width_arg = 0;
+	fmt_info->width_set = 0;
+	fmt_info->precision = 0;
+	fmt_info->precision_arg = 0;
+	fmt_info->precision_set = 0;
+	fmt_info->length = 0;
+	fmt_info->length_set = 0;
+	fmt_info->specifier = 0;
+}
+
+/** Attempts to parse a hex field
+ * in a format string. If the function
+ * is successfull, it returns the the
+ * number of characters parsed. If it
+ * fails, it returns zero.
+ * */
+
+static unsigned int fmt_parse(struct fmt_info *fmt_info, const char *fmt, va_list args) {
+
+	unsigned int i = 0;
+
+	if (fmt[i] != '%')
+		return 0;
+	else
+		i++;
+
+	/* Parse flags */
+
+	fmt_info->flags = 0;
+
+	while (fmt[i] != 0) {
+
+		if (fmt[i] == '-') {
+			fmt_info->flags |= FLAG_LEFT_JUSTIFY;
+		} else if (fmt[i] == '+') {
+			fmt_info->flags |= FLAG_ADD_SIGN;
+		} else if (fmt[i] == ' ') {
+			fmt_info->flags |= FLAG_SPACE_OR_SIGN;
+		} else if (fmt[i] == '#') {
+			fmt_info->flags |= FLAG_HASH_TAG;
+		} else if (fmt[i] == '0') {
+			fmt_info->flags |= FLAG_PAD_ZERO;
+		} else {
+			break;
+		}
+
+		i++;
 	}
+
+	/* Parse width */
+
+	if (pure64_isdigit(fmt[i]) && (fmt[i] != '0')) {
+		/* Print at least this many
+		 * characters. */
+		fmt_info->width = fmt[i] - '0';
+		fmt_info->width_arg = 0;
+		fmt_info->width_set = 1;
+		i++;
+	} else if (fmt[i] == '*') {
+		/* The width specifier is in
+		 * an argument. */
+		fmt_info->width = 0;
+		fmt_info->width_arg = 1;
+		fmt_info->width_set = 1;
+		i++;
+	} else {
+		/* No width specifier was given. */
+		fmt_info->width = 0;
+		fmt_info->width_set = 0;
+	}
+
+	/* Get the width argument. */
+
+	if (fmt_info->width_set && fmt_info->width_arg) {
+		fmt_info->width = va_arg(args, unsigned int);
+	}
+
+	/* Parse precision */
+
+	if (fmt[i] == '.') {
+
+		i++;
+
+		if (pure64_isdigit(fmt[i])) {
+			fmt_info->precision = fmt[i] - '0';
+			fmt_info->precision_arg = 0;
+			fmt_info->precision_set = 1;
+			i++;
+		} else if (fmt[i] == '*') {
+			fmt_info->precision = 0;
+			fmt_info->precision_arg = 1;
+			fmt_info->precision_set = 1;
+			i++;
+		} else {
+			/* There should have been a
+			 * precision specifier here.
+			 * Bail out with and return zero
+			 * to indicate failure. */
+			fmt_info->precision = 0;
+			fmt_info->precision_arg = 0;
+			fmt_info->precision_set = 0;
+			return 0;
+		}
+
+		/* Get the precision argument. */
+
+		if (fmt_info->precision_set && fmt_info->precision_arg) {
+			fmt_info->precision = va_arg(args, unsigned int);
+		}
+	}
+
+	/* Parse length */
+
+	if ((fmt[i + 0] == 'h')
+	 && (fmt[i + 1] == 'h')) {
+		fmt_info->length = LENGTH_hh;
+		fmt_info->length_set = 1;
+		i += 2;
+	} else if (fmt[i] == 'h') {
+		fmt_info->length = LENGTH_h;
+		fmt_info->length_set = 1;
+		i++;
+	} else if ((fmt[i + 0] == 'l')
+	        && (fmt[i + 1] == 'l')) {
+		fmt_info->length = LENGTH_ll;
+		fmt_info->length_set = 1;
+		i += 2;
+	} else if (fmt[i] == 'l') {
+		fmt_info->length = LENGTH_l;
+		fmt_info->length_set = 1;
+		i++;
+	} else if (fmt[i] == 'j') {
+		fmt_info->length = LENGTH_j;
+		fmt_info->length_set = 1;
+		i++;
+	} else if (fmt[i] == 'z') {
+		fmt_info->length = LENGTH_z;
+		fmt_info->length_set = 1;
+		i++;
+	} else if (fmt[i] == 't') {
+		fmt_info->length = LENGTH_t;
+		fmt_info->length_set = 1;
+		i++;
+	} else if (fmt[i] == 'L') {
+		fmt_info->length = LENGTH_L;
+		fmt_info->length_set = 1;
+		i++;
+	} else {
+		fmt_info->length = 0;
+		fmt_info->length_set = 0;
+	}
+
+	/* Parse specifier */
+
+	if ((fmt[i] == 'd') || (fmt[i] == 'i')) {
+		fmt_info->specifier = SPECIFIER_d;
+	} else if (fmt[i] == 'u') {
+		fmt_info->specifier = SPECIFIER_u;
+	} else if (fmt[i] == 'x') {
+		fmt_info->specifier = SPECIFIER_x;
+	} else if (fmt[i] == 'X') {
+		fmt_info->specifier = SPECIFIER_X;
+	} else if (fmt[i] == 'c') {
+		fmt_info->specifier = SPECIFIER_c;
+	} else if (fmt[i] == 's') {
+		fmt_info->specifier = SPECIFIER_s;
+	} else if (fmt[i] == 'p') {
+		fmt_info->specifier = SPECIFIER_p;
+	} else {
+		/* Unsupported field specifier */
+		return 0;
+	}
+
+	i++;
+
+	return i;
+}
+
+static void debug_print_string(const char *str, unsigned int len) {
+
+	unsigned int i;
+
+	for (i = 0; i < len; i++)
+		debug_putc(str[i]);
+}
+
+static void fmt_print_hex(const struct fmt_info *fmt_info, va_list args) {
+
+	const char hex[16] = "0123456789abcdef";
+	char str[8];
+	unsigned int n;
+	unsigned int i;
+
+	n = va_arg(args, unsigned int);
+
+	str[0] = hex[(n >> 28) & 0xf];
+	str[1] = hex[(n >> 24) & 0xf];
+
+	str[2] = hex[(n >> 20) & 0xf];
+	str[3] = hex[(n >> 16) & 0xf];
+
+	str[4] = hex[(n >> 12) & 0xf];
+	str[5] = hex[(n >> 8) & 0xf];
+
+	str[6] = hex[(n >> 4) & 0xf];
+	str[7] = hex[(n >> 0) & 0xf];
+
+	/* Print leading zeros or
+	 * spaces first. */
+
+	for (i = 0; i < 8; i++) {
+		if (str[i] == '0') {
+			if (fmt_info->width_set && ((8 - i) < fmt_info->width)) {
+				if (fmt_info->flags & FLAG_PAD_ZERO)
+					debug_putc('0');
+				else
+					debug_putc(' ');
+			}
+		} else {
+			break;
+		}
+	}
+
+	while (i < 8) {
+		debug_putc(str[i]);
+		i++;
+	}
+}
+
+static void fmt_print_string(const struct fmt_info *fmt_info, va_list args) {
+
+	char *str;
+	unsigned int len;
+
+	str = va_arg(args, char *);
+
+	if (fmt_info->precision_set)
+		len = fmt_info->precision;
+	else
+		len = pure64_strlen(str);
+
+	debug_print_string(str, len);
+}
+
+static void fmt_print(const struct fmt_info *fmt_info, va_list args) {
+
+	if (fmt_info->specifier == SPECIFIER_x) {
+		fmt_print_hex(fmt_info, args);
+	} else if (fmt_info->specifier == SPECIFIER_s) {
+		fmt_print_string(fmt_info, args);
+	}
+}
+
+void debug(const char *fmt, ...) {
+
+	unsigned int i;
+	unsigned int ret;
+	struct fmt_info fmt_info;
+	va_list args;
+
+	fmt_info_init(&fmt_info);
+
+	va_start(args, fmt);
+
+	for (i = 0; fmt[i]; i++) {
+		ret = fmt_parse(&fmt_info, &fmt[i], args);
+		if (ret > 0) {
+			i += ret - 1;
+			fmt_print(&fmt_info, args);
+		} else if ((fmt[i] == '%') && (fmt[i + 1] == '%')) {
+			debug_putc('%');
+			i++;
+		} else {
+			debug_putc(fmt[i]);
+		}
+	}
+
+	va_end(args);
 }
