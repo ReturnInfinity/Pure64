3586f48b674b81e6557c80c6bc1f4c15225e0437
Taylor Holberton <taylorcholberton@gmail.com>
2018-02-20 22:18:32 -0500

    added pure64_strerror()

--- src/lib/error.c
+++ src/lib/error.c
@@ -4,6 +4,37 @@
  * =============================================================================
  */
 
+#include <pure64/error.h>
+
+const char *pure64_strerror(int err) {
+
+	switch (err) {
+	case 0:
+		/* Zero indicates no error */
+		break;
+	case PURE64_EFAULT:
+		return "Bad address was detected.";
+	case PURE64_ENOMEM:
+		return "Not enough memory available.";
+	case PURE64_EISDIR:
+		return "Entry is a directory.";
+	case PURE64_ENOTDIR:
+		return "Entry is not a directory.";
+	case PURE64_EEXIST:
+		return "Entry exists already.";
+	case PURE64_EINVAL:
+		return "Invalid argument was detected.";
+	case PURE64_ENOSYS:
+		return "Functionality not implemented.";
+	case PURE64_EIO:
+		return "I/O error occured";
+	default:
+		return "Unknown error has occurred.";
+	}
+
+	return "Success";
+}
+
 /** @file */
 
 #ifndef PURE64_ERROR_H
