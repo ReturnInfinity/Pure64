c77b85f011c27f15bb93e106ed640a948dda24c9
Taylor Holberton <taylorcholberton@gmail.com>
2018-02-24 07:36:27 -0500

    put UUID functions in library

---
+++ include/pure64/uuid.h
@@ -0,0 +1,72 @@
+/* =============================================================================
+ * Pure64 -- a 64-bit OS/software loader written in Assembly for x86-64 systems
+ * Copyright (C) 2008-2017 Return Infinity -- see LICENSE.TXT
+ * =============================================================================
+ */
+
+/** @file */
+
+#ifndef PURE64_UUID_H
+#define PURE64_UUID_H
+
+#ifndef PURE64_UUID_DISK
+#define PURE64_UUID_DISK "dc301a8c-1632-45a6-962c-aa4f1cc35943"
+#endif
+
+#ifndef PURE64_UUID_PARTITION
+#define PURE64_UUID_PARTITION "32cfd7f2-0e0a-4908-8d3b-16d7fb3a3c57"
+#endif
+
+#ifndef PURE64_UUID_INIT
+#define PURE64_UUID_INIT "daa1ab4e-7a2c-4404-8208-61a12c660382"
+#endif
+
+#ifndef PURE64_UUID_FS
+#define PURE64_UUID_FS "f7439905-43da-4df0-b863-1f456e008b58"
+#endif
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Universally unique identifer.
+ * Used to identify various parts
+ * of the Pure64 partition table.
+ * */
+
+struct pure64_uuid {
+	/** The byte oriented layout
+	 * of the identifier. */
+	uint8_t bytes[16];
+};
+
+/** Zero the value of an identifier.
+ * This may be desired for initialization
+ * purposes. A UUID of all zeros indicates
+ * that it was not initialized properly.
+ * @param uuid A UUID structure.
+ * */
+
+void pure64_uuid_zero(struct pure64_uuid *uuid);
+
+/** Parse a UUID string.
+ * @param uuid A UUID structure.
+ * @param uuid_str A UUID string.
+ * The first three numbers should be
+ * little endian and the last two should
+ * be big endian. It does not matter if
+ * the hex characters are upper or lower case.
+ * @returns Zero on success, @ref PURE64_EINVAL
+ * if the string was formatted incorrectly, and
+ * @ref PURE64_EFAULT if an address was NULL.
+ * */
+
+int pure64_uuid_parse(struct pure64_uuid *uuid, const char *uuid_str);
+
+#ifdef __cplusplus
+} /* extern "C" { */
+#endif
+
+#endif /* PURE64_UUID_H */
--- src/util/pure64.c
+++ src/util/pure64.c
@@ -7,6 +7,7 @@
 #include <pure64/fs.h>
 #include <pure64/file.h>
 #include <pure64/error.h>
+#include <pure64/uuid.h>
 #include <pure64/stream.h>
 
 #include "mbr-data.h"
@@ -231,89 +232,6 @@ static int calculate_checksums(FILE *file) {
 	}
 
 	return EXIT_SUCCESS;
-}
-
-/* * * * * * * * * *
- * UUID Declarations
- * * * * * * * * * */
-
-struct pure64_uuid {
-	uint8_t bytes[16];
-};
-
-static int pure64_uuid_parse(struct pure64_uuid *uuid, const char *str) {
-
-	unsigned int i;
-	unsigned int j;
-
-	i = 0;
-	j = 0;
-
-	while ((i < 16) && (str[j] != 0)) {
-
-		uuid->bytes[i] = 0;
-
-		char c = tolower(str[j]);
-		if ((c >= '0') && (c <= '9'))
-			uuid->bytes[i] |= ((c - '0') + 0x00) << 4;
-		else if ((c >= 'a') && (c <= 'f'))
-			uuid->bytes[i] |= ((c - 'a') + 0x0a) << 4;
-		else if (c == '-') {
-			j++;
-			continue;
-		} else
-			return -1;
-
-		c = tolower(str[j + 1]);
-		if ((c >= '0') && (c <= '9'))
-			uuid->bytes[i] |= (c - '0') + 0x00;
-		else if ((c >= 'a') && (c <= 'f'))
-			uuid->bytes[i] |= (c - 'a') + 0x0a;
-		else
-			return -1;
-
-		j += 2;
-		i++;
-	}
-
-	/* adjust for little endian encoding */
-
-	unsigned char buf[16];
-
-	/* little endian */
-
-	buf[0] = uuid->bytes[3];
-	buf[1] = uuid->bytes[2];
-	buf[2] = uuid->bytes[1];
-	buf[3] = uuid->bytes[0];
-
-	/* little endian */
-
-	buf[4] = uuid->bytes[5];
-	buf[5] = uuid->bytes[4];
-
-	/* little endian */
-
-	buf[6] = uuid->bytes[7];
-	buf[7] = uuid->bytes[6];
-
-	/* big endian */
-
-	buf[8] = uuid->bytes[8];
-	buf[9] = uuid->bytes[9];
-
-	/* big endian */
-
-	buf[10] = uuid->bytes[10];
-	buf[11] = uuid->bytes[11];
-	buf[12] = uuid->bytes[12];
-	buf[13] = uuid->bytes[13];
-	buf[14] = uuid->bytes[14];
-	buf[15] = uuid->bytes[15];
-
-	for (i = 0; i < 16; i++)
-		uuid->bytes[i] = buf[i];
-	return 0;
 }
 
 /* * * * * * * * * * * * *
