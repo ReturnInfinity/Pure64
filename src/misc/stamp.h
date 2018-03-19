6540facec68dca6dc070ce9987242c07bc7ea655
Taylor Holberton <taylorcholberton@gmail.com>
2018-03-19 12:06:53 -0400

    added copyright stamp header

---
+++ src/misc/stamp.h
@@ -0,0 +1,5 @@
+/* =============================================================================
+ * Pure64 -- a 64-bit OS/software loader written in Assembly for x86-64 systems
+ * Copyright (C) 2008-2017 Return Infinity -- see LICENSE.TXT
+ * =============================================================================
+ */
