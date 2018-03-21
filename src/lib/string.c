d29fcf7f3ee905b555005a1756293fa0614fb12d
Taylor Holberton <taylorcholberton@gmail.com>
2018-03-20 21:53:20 -0400

    updated types

--- src/lib/string.c
+++ src/lib/string.c
@@ -6,10 +6,10 @@
 
 #include <pure64/string.h>
 
-void pure64_memset(void *dst, int value, unsigned long int size) {
+void pure64_memset(void *dst, int value, pure64_uint64 size) {
 
 	unsigned char *dst8;
-	unsigned long int i;
+	pure64_uint64 i;
 
 	dst8 = (unsigned char *) dst;
 
@@ -18,11 +18,11 @@ void pure64_memset(void *dst, int value, unsigned long int size) {
 	}
 }
 
-void pure64_memcpy(void *dst, const void *src, unsigned long int size) {
+void pure64_memcpy(void *dst, const void *src, pure64_uint64 size) {
 
 	unsigned char *dst8;
 	const unsigned char *src8;
-	unsigned long int i;
+	pure64_uint64 i;
 
 	dst8 = (unsigned char *) dst;
 	src8 = (const unsigned char *) src;
@@ -32,12 +32,12 @@ void pure64_memcpy(void *dst, const void *src, unsigned long int size) {
 	}
 }
 
-int pure64_memcmp(const void *a, const void *b, unsigned long int size) {
+int pure64_memcmp(const void *a, const void *b, pure64_uint64 size) {
 
 	const unsigned char *a8 = (const unsigned char *) a;
 	const unsigned char *b8 = (const unsigned char *) b;
 
-	for (unsigned long int i = 0; i < size; i++) {
+	for (pure64_uint64 i = 0; i < size; i++) {
 		if (a8[i] < b8[i])
 			return -1;
 		else if (a8[i] > b8[i])
@@ -47,9 +47,9 @@ int pure64_memcmp(const void *a, const void *b, unsigned long int size) {
 	return 0;
 }
 
-unsigned long int pure64_strlen(const char *str) {
+pure64_uint64 pure64_strlen(const char *str) {
 
-	unsigned long int i = 0;
+	pure64_uint64 i = 0;
 
 	while (str[i] != 0)
 		i++;
