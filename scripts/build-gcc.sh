5e870d540ce93d1e96206cd55f39b7929901b489
Taylor Holberton <taylorcholberton@gmail.com>
2018-03-29 21:54:56 -0400

    added cross compiler build to travis

---
+++ scripts/build-gcc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ver="7.3.0"
+url="https://ftp.gnu.org/gnu/gcc/gcc-$ver/gcc-$ver.tar.gz"
+
+if [ ! -e "gcc-$ver.tar.gz" ]; then
+	wget $url
+fi
+
+if [ ! -e "gcc-$ver" ]; then
+	tar --extract --file "gcc-$ver.tar.gz"
+fi
+
+builddir=gcc-$ver/build/$target
+
+mkdir -p $builddir
+cd $builddir
+../../configure --target $target --disable-nls --enable-languages=c --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
---
+++ scripts/build-toolchains.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export prefix=~/.local
+
+export target=mips64-none-elf
+scripts/build-bintutils.sh
+scripts/build-gcc.sh
+
+#export target=x86_64-none-elf
+#scripts/build-bintutils.sh
+#scripts/build-gcc.sh
+
+#export target=riscv64-none-elf
+#scripts/build-binutils.sh
+#scripts/build-gcc.sh
