2b2efcabeeafa6fe620f77ef34412bf86fe3ab05
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-02 16:33:25 -0400

    temporarily disabling riscv64 compiler build

--- scripts/build-toolchains.sh
+++ scripts/build-toolchains.sh
@@ -6,6 +6,7 @@ export target=x86_64-none-elf
 scripts/build-bintutils.sh
 scripts/build-gcc.sh
 
-export target=riscv64-none-elf
-scripts/build-binutils.sh
-scripts/build-gcc.sh
+# Disabling for now
+#export target=riscv64-none-elf
+#scripts/build-binutils.sh
+#scripts/build-gcc.sh
