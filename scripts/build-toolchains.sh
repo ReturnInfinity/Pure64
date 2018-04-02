1bbd5042709e63f4b71ea4c06a347c3abf0da5bb
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-02 18:05:05 -0400

    building toolchain only if it doesn't exist

--- scripts/build-toolchains.sh
+++ scripts/build-toolchains.sh
@@ -2,11 +2,11 @@
 
 export prefix=$PWD/toolchains
 
-#if [ ! -e "$prefix/x86_64-none-elf" ]; then
+if [ ! -e "$prefix/x86_64-none-elf" ]; then
 	export target=x86_64-none-elf
 	scripts/build-bintutils.sh
 	scripts/build-gcc.sh
-#fi
+fi
 
 # Disabling for now
 #export target=riscv64-none-elf
