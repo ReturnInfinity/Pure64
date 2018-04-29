5fe23d39374c9b9e141451031511d3a6f5f1b57c
Taylor Holberton <taylorcholberton@gmail.com>
2018-04-29 19:19:45 -0400

    documented types

--- include/pure64/core/types.h
+++ include/pure64/core/types.h
@@ -4,6 +4,10 @@
  * =============================================================================
  */
 
+/** @file types.h
+ * Common types and values used throughout the library.
+ * */
+
 #ifndef PURE64_TYPES_H
 #define PURE64_TYPES_H
 
@@ -11,31 +15,59 @@
 extern "C" {
 #endif
 
-#ifndef pure64_true
+/** A boolean true value.
+ * @ingroup core-api
+ * */
+
 #define pure64_true 1
-#endif
 
-#ifndef pure64_false
+/** A boolean false  value.
+ * @ingroup core-api
+ * */
+
 #define pure64_false 0
-#endif
 
-#ifndef pure64_null
+/** An invalid pointer value.
+ * @ingroup core-api
+ * */
+
 #define pure64_null ((void *) 0x00)
-#endif
 
-#ifndef NULL
-#define NULL pure64_null
-#endif
+/** The largest type available in the system.
+ * @ingroup core-api
+ * */
 
 typedef unsigned long long int pure64_size;
 
+/** A 64-bit unsigned integer.
+ * @ingroup core-api
+ * */
+
 typedef unsigned long long int pure64_uint64;
+
+/** A 32-bit unsigned integer.
+ * @ingroup core-api
+ * */
 
 typedef unsigned int pure64_uint32;
 
+/** A 16-bit unsigned integer.
+ * @ingroup core-api
+ * */
+
 typedef unsigned short int pure64_uint16;
 
+/** An 8-bit unsigned integer.
+ * @ingroup core-api
+ * */
+
 typedef unsigned char pure64_uint8;
+
+/** A boolean type.
+ * Use @ref pure64_false and @ref pure64_true
+ * for setting this value.
+ * @ingroup core-api
+ * */
 
 typedef unsigned char pure64_bool;
 
