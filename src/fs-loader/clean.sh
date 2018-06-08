0e422cf3302a63af311047bedd5ea6df02eee240
Taylor Holberton <taylorcholberton@gmail.com>
2018-06-08 15:46:36 -0400

    updated build artifact paths

--- src/fs-loader/clean.sh
+++ src/fs-loader/clean.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-rm -f ahci.o
-rm -f debug.o
-rm -f e820.o
-rm -f hooks.o
-rm -f map.o
-rm -f pci.o
-rm -f _start.o
-rm -f stage-three
-rm -f stage-three.sys
+rm -f *.o
+rm -f fs-loader
+rm -f fs-loader.sys
