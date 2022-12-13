f5668e9177384d3526e455a59196288951a1b5a0
Ian Seyler <ian@ians-macbook-air.local>
2022-12-13 15:10:50 +0000

    UEFI build

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ cd src
 
 nasm pure64.asm -o ../bin/pure64.sys -l ../bin/pure64-debug.txt
 
-cd bootsectors
+cd boot
 
 nasm mbr.asm -o ../../bin/mbr.sys
 nasm pxestart.asm -o ../../bin/pxestart.sys
 nasm multiboot.asm -o ../../bin/multiboot.sys
 nasm multiboot2.asm -o ../../bin/multiboot2.sys
+nasm uefi.asm -o ../../bin/uefi.sys
 
 cd ../..
