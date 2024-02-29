f75f21d5858a8f686cd0b91816e6a6cc98f4da3c
Ian <ian@ians-mac-pro.local>
2024-02-28 21:53:41 -0500

    Cleanup of init_cpu

--- src/init/cpu.asm
+++ src/init/cpu.asm
@@ -109,53 +109,44 @@ avx_not_supported:
 	test rsi, rsi
 	je noMP				; Skip MP init if we didn't get a valid LAPIC address
 
-	xor eax, eax			; Clear Task Priority (bits 7:4) and Priority Sub-Class (bits 3:0)
-	mov dword [rsi+0x80], eax	; Task Priority Register (TPR)
+	mov ecx, APIC_TPR
+	mov eax, 0x00000020
+	call apic_write			; Disable softint delivery
 
-	mov eax, 0x01000000		; Set bits 31-24 for all cores to be in Group 1
-	mov dword [rsi+0xD0], eax	; Logical Destination Register
+	mov ecx, APIC_LVT_TMR
+	mov eax, 0x00010000
+	call apic_write			; Disable timer interrupts
 
-	xor eax, eax
-	not eax				; Set EAX to 0xFFFFFFFF; Bits 31-28 set for Flat Mode
-	mov dword [rsi+0xE0], eax	; Destination Format Register
+	mov ecx, APIC_LVT_PERF
+	mov eax, 0x00010000
+	call apic_write			; Disable performance counter interrupts
 
-	mov eax, dword [rsi+0xF0]	; Spurious Interrupt Vector Register
-	mov al, 0xF8
-	bts eax, 8			; Enable APIC (Set bit 8)
-	mov dword [rsi+0xF0], eax
+;	mov eax, 0x01000000		; Set bits 31-24 for all cores to be in Group 1
+;	mov dword [rsi+0xD0], eax	; Logical Destination Register
 
-	mov eax, dword [rsi+0x320]	; LVT Timer Register
-	bts eax, 16			; Set bit 16 for mask interrupts
-	mov dword [rsi+0x320], eax
+;	xor eax, eax
+;	not eax				; Set EAX to 0xFFFFFFFF; Bits 31-28 set for Flat Mode
+;	mov dword [rsi+0xE0], eax	; Destination Format Register
 
-;	mov eax, dword [rsi+0x350]	; LVT LINT0 Register
-;	mov al, 0			;Set interrupt vector (bits 7:0)
-;	bts eax, 8			;Delivery Mode (111b==ExtlNT] (bits 10:8)
-;	bts eax, 9
-;	bts eax, 10
-;	bts eax, 15			;bit15:Set trigger mode to Level (0== Edge, 1== Level)
-;	btr eax, 16			;bit16:unmask interrupts (0==Unmasked, 1== Masked)
-;	mov dword [rsi+0x350], eax
+	mov ecx, APIC_SPURIOUS
+	mov eax, 0x000001FF
+	call apic_write			; Enable the APIC (bit 8) and set spurious vector to 0xFF
 
-;	mov eax, dword [rsi+0x360]	; LVT LINT1 Register
-;	mov al, 0			;Set interrupt vector (bits 7:0)
-;	bts eax, 8			;Delivery Mode (111b==ExtlNT] (bits 10:8)
-;	bts eax, 9
-;	bts eax, 10
-;	bts eax, 15			;bit15:Set trigger mode to Edge (0== Edge, 1== Level)
-;	btr eax, 16			;bit16:unmask interrupts (0==Unmasked, 1== Masked)
-;	mov dword [rsi+0x360], eax
+	mov ecx, APIC_LVT_LINT0
+	mov eax, 0x00008700		; Bit 15 (1 = Level), Bits 10:8 for Ext
+	call apic_write			; Enable normal external interrupts
 
-;	mov eax, dword [rsi+0x370]	; LVT Error Register
-;	mov al, 0			;Set interrupt vector (bits 7:0)
-;	bts eax, 16			;bit16:Mask interrupts (0==Unmasked, 1== Masked)
-;	mov dword [rsi+0x370], eax
+	mov ecx, APIC_LVT_LINT1
+	mov eax, 0x00000400
+	call apic_write			; Enable normal NMI processing
+
+	mov ecx, APIC_LVT_ERR
+	mov eax, 0x00010000
+	call apic_write			; Disable error interrupts
 
 	lock inc word [cpu_activated]
-	xor eax, eax
-	mov rsi, [os_LocalAPICAddress]
-	add rsi, 0x20			; Add the offset for the APIC ID location
-	lodsd				; APIC ID is stored in bits 31:24
+	mov ecx, APIC_ID
+	call apic_read			; APIC ID is stored in bits 31:24
 	shr rax, 24			; AL now holds the CPU's APIC ID (0 - 255)
 	mov rdi, 0x00005700		; The location where the cpu values are stored
 	add rdi, rax			; RDI points to InfoMap CPU area + APIC ID. ex 0x5701 would be APIC ID 1
@@ -163,6 +154,68 @@ avx_not_supported:
 	stosb
 
 	ret
+
+; -----------------------------------------------------------------------------
+; apic_read -- Read from a register in the APIC
+;  IN:	ECX = Register to read
+; OUT:	EAX = Register value
+;	All other registers preserved
+apic_read:
+	push rsi
+	mov rsi, [os_LocalAPICAddress]
+	add rsi, rcx			; Add offset
+	lodsd
+	pop rsi
+	ret
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; apic_write -- Write to a register in the APIC
+;  IN:	ECX = Register to write
+;	EAX = Value to write
+; OUT:	All registers preserved
+apic_write:
+	push rdi
+	mov rdi, [os_LocalAPICAddress]
+	add rdi, rcx			; Add offset
+	stosd
+	pop rdi
+	ret
+; -----------------------------------------------------------------------------
+
+
+; Register list
+; 0x000 - 0x010 are Reserved
+APIC_ID		equ 0x020		; ID Register
+APIC_VER	equ 0x030		; Version Register
+; 0x040 - 0x070 are Reserved
+APIC_TPR	equ 0x080		; Task Priority Register
+APIC_APR	equ 0x090		; Arbitration Priority Register
+APIC_PPR	equ 0x0A0		; Processor Priority Register
+APIC_EOI	equ 0x0B0		; End Of Interrupt
+APIC_RRD	equ 0x0C0		; Remote Read Register
+APIC_LDR	equ 0x0D0		; Logical Destination Register
+APIC_DFR	equ 0x0E0		; Destination Format Register
+APIC_SPURIOUS	equ 0x0F0		; Spurious Interrupt Vector Register
+APIC_ISR	equ 0x100		; In-Service Register (Starting Address)
+APIC_TMR	equ 0x180		; Trigger Mode Register (Starting Address)
+APIC_IRR	equ 0x200		; Interrupt Request Register (Starting Address)
+APIC_ESR	equ 0x280		; Error Status Register
+; 0x290 - 0x2E0 are Reserved
+APIC_ICRL	equ 0x300		; Interrupt Command Register (low 32 bits)
+APIC_ICRH	equ 0x310		; Interrupt Command Register (high 32 bits)
+APIC_LVT_TMR	equ 0x320		; LVT Timer Register
+APIC_LVT_TSR	equ 0x330		; LVT Thermal Sensor Register
+APIC_LVT_PERF	equ 0x340		; LVT Performance Monitoring Counters Register
+APIC_LVT_LINT0	equ 0x350		; LVT LINT0 Register
+APIC_LVT_LINT1	equ 0x360		; LVT LINT1 Register
+APIC_LVT_ERR	equ 0x370		; LVT Error Register
+APIC_TMRINITCNT	equ 0x380		; Initial Count Register (for Timer)
+APIC_TMRCURRCNT	equ 0x390		; Current Count Register (for Timer)
+; 0x3A0 - 0x3D0 are Reserved
+APIC_TMRDIV	equ 0x3E0		; Divide Configuration Register (for Timer)
+; 0x3F0 is Reserved
 
 
 ; =============================================================================
