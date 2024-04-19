ebd166f1d1dac1663c772e06ccecc80aef132c02
Ian <ian@ians-mac-pro.local>
2024-04-19 15:32:18 -0400

    update build

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ nasm pure64.asm -o ../bin/pure64.sys -l ../bin/pure64-debug.txt
 
 cd boot
 
+nasm bios.asm -o ../../bin/bios.sys -l ../../bin/bios-debug.txt
 nasm uefi.asm -o ../../bin/uefi.sys -l ../../bin/uefi-debug.txt
 
 cd ../..
