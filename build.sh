d2e15fd74967ed759227afc2944797847c260e58
isoux <isoux@isoux-xeon>
2024-09-29 17:45:48 +0300

    Added file bios-floppy.asm

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ cd boot
 
 nasm bios.asm -o ../../bin/bios.sys -l ../../bin/bios-debug.txt
 nasm uefi.asm -o ../../bin/uefi.sys -l ../../bin/uefi-debug.txt
+nasm bios-floppy.asm -o ../../bin/bios-floppy.sys -l ../../bin/bios-floppy-debug.txt
 
 cd ../..
